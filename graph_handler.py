4467ecdca09d205ed37e4a7cf1ca24a2eb2e3593
Julio <julio.c.filho@hotmail.com>
2022-02-05 20:34:44 -0300

    date time new df wrong

--- graph_handler.py
+++ graph_handler.py
@@ -14,17 +14,21 @@ class GraphHandler:
         self.main()
 
     def main(self):
-        self.ani = FuncAnimation(plt.gcf(), self.animate, interval=1000)
-        plt.ion()
-        plt.pause(0.001)
+        # plt.ion()z
+        # self.ani = FuncAnimation(plt.gcf(), self.animate, interval=1000)
+        # plt.pause(0.0001)
+        plt.show(block=False)
 
     def update_trader(self, candle):
         self.trader.update(candle)
 
-    def animate(self, i):
-        # plt.cla()
+    def animate(self):
+        plt.cla()
+        print(f'last {self.df.closeTime.head(1)}')
         plt.plot(self.df.closeTime.tail(15), self.df.close.tail(15))
-        plt.pause(0.001)
+        plt.draw()
+        plt.pause(0.0001)
 
     def update_df(self, new_df):
-        self.df.append(new_df)
+        self.df.append(new_df, ignore_index=True)
+        self.animate()
